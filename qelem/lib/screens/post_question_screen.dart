f7cb0b304f969283f06e0a50cc88c3b47dc20f7f
Bruk Tedla <biruk.tedla93@gmail.com>
2022-05-15 02:52:35 +0300

    rermove unnecessary widget

--- qelem/lib/screens/post_question_screen.dart
+++ qelem/lib/screens/post_question_screen.dart
@@ -16,37 +16,35 @@ class _PostQuestionScreenState extends State<PostQuestionScreen> {
       ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 24.0),
-        child: Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              TextFormField(
-                initialValue: '',
-                decoration: const InputDecoration(
-                  labelText: 'Topic',
-                  border: OutlineInputBorder(),
-                ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            TextFormField(
+              initialValue: '',
+              decoration: const InputDecoration(
+                labelText: 'Topic',
+                border: OutlineInputBorder(),
               ),
-              const SizedBox(height: 16.0),
-              TextFormField(
-                initialValue: '',
-                decoration: const InputDecoration(
-                  labelText: 'Content',
-                  border: OutlineInputBorder(),
-                  alignLabelWithHint: true,
-                ),
-                maxLines: 10,
-                textAlignVertical: TextAlignVertical.top,
+            ),
+            const SizedBox(height: 16.0),
+            TextFormField(
+              initialValue: '',
+              decoration: const InputDecoration(
+                labelText: 'Content',
+                border: OutlineInputBorder(),
+                alignLabelWithHint: true,
               ),
-              const SizedBox(height: 25.0),
-              ElevatedButton(
-                onPressed: () {
-                  // Respond to button press
-                },
-                child: const Text('POST'),
-              ),
-            ],
-          ),
+              maxLines: 10,
+              textAlignVertical: TextAlignVertical.top,
+            ),
+            const SizedBox(height: 25.0),
+            ElevatedButton(
+              onPressed: () {
+                // Respond to button press
+              },
+              child: const Text('POST'),
+            ),
+          ],
         ),
       ),
     );
