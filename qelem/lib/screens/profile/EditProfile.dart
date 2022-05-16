14c4310353bff968192c8eb375d23a57646c2ae9
Bruk Tedla <biruk.tedla93@gmail.com>
2022-05-16 23:54:56 +0300

    fix file path

--- qelem/lib/screens/profile/EditProfile.dart
+++ qelem/lib/screens/profile/EditProfile.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:qelem/common/Constants.dart';
+import 'package:qelem/common/constants.dart';
 
 class EditProfile extends StatefulWidget {
   const EditProfile({Key? key}) : super(key: key);
