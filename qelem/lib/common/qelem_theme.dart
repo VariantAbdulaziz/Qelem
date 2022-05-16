30734c95d13f183a36b73c6a3c98a906f6ed556e
Bruk Tedla <40060643+brukted@users.noreply.github.com>
2022-05-16 22:31:14 +0300

    Update qelem_theme.dart

--- qelem/lib/common/qelem_theme.dart
+++ qelem/lib/common/qelem_theme.dart
@@ -11,10 +11,8 @@ class QelemTheme {
         bodyText2: GoogleFonts.comfortaa(color: Colors.black87)),
     primarySwatch: AppPalette.qelemPurple,
     bottomNavigationBarTheme: BottomNavigationBarThemeData(
-      selectedLabelStyle:
-          GoogleFonts.comfortaa(color: Colors.white, fontSize: 13),
-      unselectedLabelStyle:
-          GoogleFonts.comfortaa(color: Colors.white.withOpacity(0.75), fontSize: 13),
+      selectedLabelStyle: const TextStyle(color: Colors.white),
+      unselectedLabelStyle: TextStyle(color: Colors.white.withOpacity(0.75)),
       selectedItemColor: Colors.white,
       unselectedItemColor: Colors.white.withOpacity(0.75),
     ),
