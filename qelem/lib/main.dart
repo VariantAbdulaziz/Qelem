5e2fc86a8d255cd392b83b6a467c918cb58ed776
Bruk Tedla <biruk.tedla93@gmail.com>
2022-05-16 13:59:10 +0300

    fix import

--- qelem/lib/main.dart
+++ qelem/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:qelem/common/constants.dart';
 import 'package:qelem/screens/Home.dart';
-import 'common/constants.dart';
 import 'common/qelem_theme.dart';
 
 void main() {
