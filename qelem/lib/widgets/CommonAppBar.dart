b878caf99704fbc66422c53b885e8746c87967d9
hello_world <haileformal@gmail.com>
2022-05-16 13:42:57 +0300

    remove unnecessary theming

--- qelem/lib/widgets/CommonAppBar.dart
+++ qelem/lib/widgets/CommonAppBar.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 AppBar commonAppBar(String _title) {
   return AppBar(
       title: Text(
         _title,
-        style: GoogleFonts.comfortaa(
-            color: Colors.white),
       )
   );
 }
