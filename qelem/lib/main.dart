74863e9a8aa2a04c16c370955fd7b3f58ee1061c
Nazrawi Demeke <dnazrawi@gmail.com>
2022-05-29 11:18:47 +0300

    setup gorouter navigation

--- qelem/lib/main.dart
+++ qelem/lib/main.dart
@@ -2,21 +2,53 @@ import 'package:flutter/material.dart';
 import 'package:qelem/screens/main_screen.dart';
 import 'common/constants.dart';
 import 'common/qelem_theme.dart';
+import 'package:go_router/go_router.dart';
+import 'screens/screens.dart';
 
 void main() {
-  runApp(const App());
+  runApp(App());
 }
 
 class App extends StatelessWidget {
-  const App({Key? key}) : super(key: key);
+  App({Key? key}) : super(key: key);
+
+  final _router = GoRouter(
+    routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) => const MainScreen(),
+      ),
+      GoRoute(
+        path: '/change-password',
+        builder: (context, state) => const ChangePasswordScreen(),
+      ),
+      GoRoute(
+        path: '/edit-profile',
+        builder: (context, state) => const EditProfileScreen(),
+      ),
+      GoRoute(
+        path: '/register',
+        builder: (context, state) => const RegistrationScreen(),
+      ),
+      GoRoute(
+        path: '/sign-in',
+        builder: (context, state) => const LoginScreen(),
+      ),
+      GoRoute(
+        path: '/post-question',
+        builder: (context, state) => const PostQuestionScreen(),
+      ),
+    ],
+  );
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: Constants.home,
       theme: QelemTheme.qelemTheme,
       debugShowCheckedModeBanner: false,
-      home: const MainScreen(),
+      routeInformationParser: _router.routeInformationParser,
+      routerDelegate: _router.routerDelegate,
     );
   }
 }
---
+++ qelem/lib/screens/screens.dart
@@ -0,0 +1,6 @@
+export './change_password_screen.dart';
+export './edit_profile_screen.dart';
+export './register_login_screen.dart';
+export './registration_screen.dart';
+export './login_screen.dart';
+export './post_question_screen.dart';
