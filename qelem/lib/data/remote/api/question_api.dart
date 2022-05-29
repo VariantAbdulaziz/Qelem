0b6d3a65f0f149970dca12f0f4cdc2b1882278a6
Abdulaziz Ali <s9uidward@gmail.com>
2022-05-29 15:44:44 +0300

    added a get question api

--- qelem/lib/data/remote/api/question_api.dart
+++ qelem/lib/data/remote/api/question_api.dart
@@ -7,7 +7,7 @@ import 'package:qelem/data/remote/models/questio_model.dart';
 class QuestionApi {
   static const String _questionsRoot = "${Constants.BASE_URL}questions/";
 
-  Future<QuestionModel> getQuestion() async {
+  Future<QuestionModel> getQuestions() async {
     var response = await http.get(Uri.parse(_questionsRoot));
 
     final Map<String, dynamic> data = json.decode(response.body);
@@ -16,6 +16,19 @@ class QuestionApi {
       return QuestionModel.fromJson(data);
     } else {
       throw Exception('Failed to load quesions');
+    }
+  }
+
+  Future<QuestionModel> getQuestion(int questionId) async {
+    var url = Uri.parse(_questionsRoot + "$questionId");
+
+    final response = await http.get(url);
+    final Map<String, dynamic> data = json.decode(response.body);
+
+    if (response.statusCode == 200) {
+      return QuestionModel.fromJson(data);
+    } else {
+      throw Exception('Failed to fetch quesion');
     }
   }
 }
