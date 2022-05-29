1904b3adb5994c888a27691b0e85a5fe9f8bc002
Abdulaziz Ali <s9uidward@gmail.com>
2022-05-29 16:11:37 +0300

    added a create an answer api

--- qelem/lib/data/remote/api/answer_api.dart
+++ qelem/lib/data/remote/api/answer_api.dart
@@ -5,10 +5,10 @@ import 'package:qelem/common/constants.dart';
 import 'package:qelem/data/remote/models/answer_model.dart';
 
 class AnswerApi {
-  static const String _answersRoot = "${Constants.BASE_URL}answers/";
+  static const String _answersRootUrl = "${Constants.BASE_URL}answers/";
 
   Future<AnswerModel> getAnswers() async {
-    var response = await http.get(Uri.parse(_answersRoot));
+    var response = await http.get(Uri.parse(_answersRootUrl));
 
     final Map<String, dynamic> data = json.decode(response.body);
 
@@ -20,7 +20,7 @@ class AnswerApi {
   }
 
   Future<AnswerModel> getQuestion(int answerId) async {
-    var url = Uri.parse(_answersRoot + "$answerId");
+    var url = Uri.parse(_answersRootUrl + "$answerId");
 
     final response = await http.get(url);
     final Map<String, dynamic> data = json.decode(response.body);
@@ -29,6 +29,24 @@ class AnswerApi {
       return AnswerModel.fromJson(data);
     } else {
       throw Exception('Failed to fetch quesion');
+    }
+  }
+
+  Future<AnswerModel> createAnswer(AnswerModel answer) async {
+    final url = Uri.parse(_answersRootUrl);
+    var body = answer.toJson();
+
+    final response = await http.post(
+      url,
+      headers: {'Content-Type': "application/json"},
+      body: body,
+    );
+
+    if (response.statusCode == 201) {
+      final Map<String, dynamic> data = json.decode(response.body);
+      return AnswerModel.fromJson(data);
+    } else {
+      throw Exception('Failed to post answer');
     }
   }
 }
