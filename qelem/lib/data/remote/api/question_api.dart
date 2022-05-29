407991beef9765547a9e75ffcebd287459e4a3d2
Abdulaziz Ali <s9uidward@gmail.com>
2022-05-29 16:25:12 +0300

    added a update question api

--- qelem/lib/data/remote/api/question_api.dart
+++ qelem/lib/data/remote/api/question_api.dart
@@ -50,4 +50,22 @@ class QuestionApi {
       throw Exception('Failed to post quesion');
     }
   }
+
+  Future<QuestionModel> updateQuestion(QuestionModel question) async {
+    final url = Uri.parse(_questionsRootUrl);
+    var body = question.toJson();
+
+    final response = await http.post(
+      url,
+      headers: {'Content-Type': "application/json"},
+      body: body,
+    );
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> data = json.decode(response.body);
+      return QuestionModel.fromJson(data);
+    } else {
+      throw Exception('Failed to update quesion');
+    }
+  }
 }
