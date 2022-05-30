dc16c48c4a0c105573ce0dc003cc9aa17af4c2b6
hello_world <haileformal@gmail.com>
2022-05-30 18:02:34 +0300

    improve profile update

--- qelem/lib/data/profile_update.dart
+++ qelem/lib/data/profile_update.dart
@@ -1,85 +1,90 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:flutter/material.dart';
 import 'package:qelem/common/constants.dart';
 import 'package:qelem/data/remote/models/user_model.dart';
+import 'package:http/http.dart' as http;
+
+import '../common/util/json_to_form_data_converter.dart';
 
 
 
 // get the user profile from the server
-Future<UserModel> getProfile(String url, String token) async {
-  HttpClient client = HttpClient();
+class ProfileNetworkService {
+  Future<UserModel> getProfile(String url, String token) async {
+    HttpClient client = HttpClient();
+    HttpClientRequest request = await client.getUrl(
+        Uri.parse(Constants.BASE_URL + url));
 
-  HttpClientRequest request = await client.getUrl(
-      Uri.parse(Constants.BASE_URL + url));
+    request.headers.add(HttpHeaders.authorizationHeader, "Bearer " + token);
 
-  request.headers.add(HttpHeaders.authorizationHeader, "Bearer " + token);
+    HttpClientResponse response = await request.close();
 
-  HttpClientResponse response = await request.close();
+    if (response.statusCode == 200) {
+      String responseBody = await response.transform(utf8.decoder).join();
+      return UserModel.fromJson(json.decode(responseBody));
 
-  if (response.statusCode == 200) {
-    String responseBody = await response.transform(utf8.decoder).join();
-    return UserModel.fromJson(json.decode(responseBody));
+    } else {
 
-  } else {
+      throw Exception('Failed to load profile!');
+    }
 
-    throw Exception('Failed to load profile!');
   }
 
-}
 
+  // update the user profile on the server from the user model object passed
+  Future<UserModel> updateProfile(String url, String token, Map<String, dynamic> dataToBeChanged, File? profilePicture) async {
 
-// update the user profile on the server from the user model object passed
-Future<UserModel> updateProfile(String url, String token, UserModel userModel) async {
-  HttpClient client = HttpClient();
+    var request = http.MultipartRequest("PATCH", Uri.parse(Constants.BASE_URL + url));
+    request.headers.addAll({"Authorization": "Bearer " + token});
 
-  HttpClientRequest request = await client.patchUrl(
-      Uri.parse(Constants.BASE_URL + url));
+    if (profilePicture != null) {
+      request.files.add(await http.MultipartFile.fromPath('profile', profilePicture.path));
+    }
 
-  request.headers.add(HttpHeaders.authorizationHeader, "Bearer " + token);
+    request = jsonToFormData(request, dataToBeChanged);
 
-  request.headers.add('content-type', 'application/json');
+    var response = await request.send();
 
-  request.add(utf8.encode(json.encode(userModel.toJson())));
+    if (response.statusCode == 200) {
 
-  HttpClientResponse response = await request.close();
+      var responseBody = await response.stream.bytesToString();
 
-  if (response.statusCode == 200) {
+      return UserModel.fromJson(json.decode(responseBody));
 
-    String responseBody = await response.transform(utf8.decoder).join();
-    return UserModel.fromJson(json.decode(responseBody));
+    } else {
 
-  } else {
+      throw Exception('Failed to update profile!');
+    }
 
-    throw Exception('Failed to update profile!');
   }
 
-}
 
+  // change password 
+  Future<UserModel> changePassword(String url, String token, String oldPassword, String newPassword) async {
+    HttpClient client = HttpClient();
 
-// change password 
-Future<UserModel> changePassword(String url, String token, String oldPassword, String newPassword) async {
-  HttpClient client = HttpClient();
+    HttpClientRequest request = await client.patchUrl(
+        Uri.parse(Constants.BASE_URL + url));
 
-  HttpClientRequest request = await client.patchUrl(
-      Uri.parse(Constants.BASE_URL + url));
+    request.headers.add(HttpHeaders.authorizationHeader, "Bearer " + token);
 
-  request.headers.add(HttpHeaders.authorizationHeader, "Bearer " + token);
+    request.headers.add('content-type', 'application/json');
+    request.add(utf8.encode(json.encode({
+      "oldPassword": oldPassword,
+      "newPassword": newPassword
+    })));
 
-  request.headers.add('content-type', 'application/json');
-  request.add(utf8.encode(json.encode({
-    "oldPassword": oldPassword,
-    "newPassword": newPassword
-  })));
+    HttpClientResponse response = await request.close();
+    if (response.statusCode == 200) {
+    
+      String responseBody = await response.transform(utf8.decoder).join();
+      return UserModel.fromJson(json.decode(responseBody));
+    
+    } else {
+      throw Exception('Failed to change password!');
+    }
 
-  HttpClientResponse response = await request.close();
-  if (response.statusCode == 200) {
-  
-    String responseBody = await response.transform(utf8.decoder).join();
-    return UserModel.fromJson(json.decode(responseBody));
-  
-  } else {
-    throw Exception('Failed to change password!');
   }
-
 }
