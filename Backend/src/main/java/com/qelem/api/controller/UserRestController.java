03af765ca36221521c9868e08659eaabf0a35775
hello_world <haileformal@gmail.com>
2022-05-30 19:28:57 +0300

    small api improvements.

--- Backend/src/main/java/com/qelem/api/controller/UserRestController.java
+++ Backend/src/main/java/com/qelem/api/controller/UserRestController.java
@@ -5,11 +5,9 @@ import java.util.List;
 import com.qelem.api.file.FileStorageConfiguration;
 import com.qelem.api.file.StorageService;
 import com.qelem.api.model.ChangePasswordModel;
-import com.qelem.api.model.RegistrationForm;
 import com.qelem.api.model.UserModel;
 import com.qelem.api.repository.UserRepository;
 import com.qelem.api.restdto.UserDto;
-import com.qelem.api.model.UserModel.ROLE;
 import com.qelem.api.util.PasswordException;
 import com.qelem.api.util.UnauthorizedAccess;
 import com.qelem.api.util.UserNotFoundException;
@@ -17,7 +15,6 @@ import com.qelem.api.util.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -31,7 +28,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -96,12 +92,14 @@ public class UserRestController {
             throw new PasswordException("Password doesn't match!");
         }
         userModel.setPassword(passwordEncoder.encode(user.getNewPassword()));
-        return userRepository.save(userModel);
+        UserModel res = userRepository.save(userModel);
+        res.setPassword("");
+        return res;
     }
 
     @PatchMapping(path = "/{id}", consumes = "application/json")
     public UserModel updateUser(@PathVariable("id") Long id,
-            @RequestBody UserModel user) {
+            @RequestBody ChangePasswordModel user) {
         var isAuthorized = loggedInUser().getId().equals(id) || loggedInUser().isAdmin();
 
         if (!isAuthorized) {
@@ -116,9 +114,6 @@ public class UserRestController {
             throw new PasswordException("Password doesn't match!");
         }
         userModel.setPassword(passwordEncoder.encode(user.getNewPassword()));
-        if (user.getProfilePicture() != null) {
-            userModel.setProfilePicture(user.getProfilePicture());
-        }
 
         return userRepository.save(userModel);
     }
@@ -182,9 +177,12 @@ public class UserRestController {
         if (user.getProfilePicture() != null && !user.getProfilePicture().isEmpty()) {
             userModel.setProfilePicture(user.getProfilePicture());
         }
-        if (user.getVote() != null && !user.getVote().isEmpty()) {
-            userModel.setVote(user.getVote());
-        }
+
+        // The user model doesn't have vote property. Why did you add it
+
+        // if (user.getVote() != null && !user.getVote().isEmpty()) {
+        //     userModel.setVote(user.getVote());
+        // }
 
         UserModel res = userRepository.save(userModel);
         res.setPassword("");
