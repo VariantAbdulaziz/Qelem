6795e1acc03e3f77842e5057ddb0b6539f9ebdb1
hello_world <haileformal@gmail.com>
2022-05-30 18:00:56 +0300

    add form converter

---
+++ qelem/lib/common/util/json_to_form_data_converter.dart
@@ -0,0 +1,8 @@
+import 'package:http/http.dart' as http;
+
+jsonToFormData(http.MultipartRequest request, Map<String, dynamic> data) {
+  for (var key in data.keys) {
+    request.fields[key] = data[key].toString();
+  }
+  return request;
+}
