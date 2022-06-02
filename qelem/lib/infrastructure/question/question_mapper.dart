b3be3a4bf93dad0c06acd033c501c769805bd2ec
hello_world <haileformal@gmail.com>
2022-06-02 09:00:29 +0300

    update mapper to match new Vote model

--- qelem/lib/infrastructure/question/question_mapper.dart
+++ qelem/lib/infrastructure/question/question_mapper.dart
@@ -1,11 +1,20 @@
 import 'package:qelem/infrastructure/auth/user_model_mapper.dart';
 import 'package:qelem/infrastructure/question/question_dto.dart';
 
+import '../../common/Vote.dart';
 import '../../domain/question/question.dart';
 
 extension QuestionMapper on QuestionDto {
 
   Question toQuestion() {
+    Vote vote;
+    if (userVote == 1) {
+      vote = Vote.upVote;
+    } else if (userVote == -1) {
+      vote = Vote.downVote;
+    } else {
+      vote = Vote.none;
+    }
     return Question(
       id: id,
       topic: topic,
@@ -13,7 +22,7 @@ extension QuestionMapper on QuestionDto {
       author: author.toUser(),
       upVotes: upVotes,
       downVotes: downVotes,
-      userVote: userVote,
+      userVote: vote,
     );
   }
 }
