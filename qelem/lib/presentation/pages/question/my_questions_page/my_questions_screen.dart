a35f15912abd2e3710893a35ac73849150882f76
hello_world <haileformal@gmail.com>
2022-06-03 00:26:18 +0300

    use myQuestion bloc

--- qelem/lib/presentation/pages/question/my_questions_page/my_questions_screen.dart
+++ qelem/lib/presentation/pages/question/my_questions_page/my_questions_screen.dart
@@ -1,29 +1,94 @@
 import 'package:flutter/material.dart';
-import 'package:qelem/common/Vote.dart';
-import 'package:qelem/domain/auth/user.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:qelem/application/question/question_bloc.dart';
 import 'package:qelem/domain/question/question.dart';
 
 import '../widgets/question_card.dart';
 
-Widget myQuestionsScreen() {
-  return ListView.builder(
-    itemCount: 20,
-    itemBuilder: (context, position) {
-      return QuestionCard(Question(
-        id: position,
-        topic: 'Question $position',
-        content: 'Content of question $position',
-        author: User(
-          id: 1,
-          userName: 'emre_varol',
-          firstName: "Emre",
-          lastName: "Varol",
-          profilePicture: "assets/images/default_profile_picture.jpeg",
-        ),
-        upVotes: 0,
-        downVotes: 0,
-        userVote: Vote.none,
-      ));
-    },
-  );
+class MyQuestionsScreen extends StatefulWidget {
+  // final User user;
+
+  const MyQuestionsScreen({Key? key}) : super(key: key);
+
+  @override
+  _MyQuestionsScreenState createState() => _MyQuestionsScreenState();
+}
+
+class _MyQuestionsScreenState extends State<MyQuestionsScreen> {
+  final _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_onScroll);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<QuestionBloc, QuestionState>(
+      builder: (context, state) {
+        if (state.status == QuestionStateStatus.error) {
+          return const Center(child: Text('Error')); // todo: a better widget
+        } else if (state.status == QuestionStateStatus.initial) {
+          BlocProvider.of<QuestionBloc>(context).add(QuestionEventFetch());
+          return const Center(child: CircularProgressIndicator());
+        } else if (state.status == QuestionStateStatus.loaded) {
+          if (state.questions.isEmpty) {
+            return const Center(child: Text('No questions'));
+          }
+          return ListView.builder(
+            itemCount: state.reachedEnd ? state.questions.length : state.questions.length + 1,
+            itemBuilder: (context, position) {
+              return position >= state.questions.length
+                  ? BottomLoader() // fetch the next 20 questions
+                  : QuestionCard(Question(
+                      id: position,
+                      topic: state.questions[position].topic,
+                      content: state.questions[position].content,
+                      author: state.questions[position].author,
+                      upVotes: state.questions[position].upVotes,
+                      downVotes: state.questions[position].downVotes,
+                      userVote: state.questions[position].userVote,
+                    ));
+            },
+            controller: _scrollController,
+          );
+        } else {
+          return const Center(child: CircularProgressIndicator());
+        }
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _scrollController
+      ..removeListener(_onScroll)
+      ..dispose();
+    super.dispose();
+  }
+
+  void _onScroll() {
+    if (_isBottom) BlocProvider.of<QuestionBloc>(context).add(QuestionEventFetch());
+  }
+
+  bool get _isBottom {
+    if (!_scrollController.hasClients) return false;
+    final maxScroll = _scrollController.position.maxScrollExtent;
+    final currentScroll = _scrollController.offset;
+    return currentScroll >= (maxScroll * 0.9);
+  }
+}
+
+class BottomLoader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: SizedBox(
+        height: 24,
+        width: 24,
+        child: CircularProgressIndicator(strokeWidth: 1.5),
+      ),
+    );
+  }
 }
