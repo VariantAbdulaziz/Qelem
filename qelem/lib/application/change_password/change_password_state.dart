54838980e079c8f68f00750b5d508aab84da369f
Biruk-Tassew <biruktassew21@gmail.com>
2022-06-02 19:27:34 -0400

    added a state file for change_password

---
+++ qelem/lib/application/change_password/change_password_state.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'change_password_state.freezed.dart';
+
+@freezed
+class ChangePasswordState with _$ChangePasswordState {
+  const factory ChangePasswordState.initial() = ChangePasswordStateInitial;
+  const factory ChangePasswordState.loading() = ChangePasswordStateLoading;
+  const factory ChangePasswordState.success() = ChangePasswordStateSuccess;
+  const factory ChangePasswordState.failure(Error error) =
+      ChangePasswordStateFailure;
+}
