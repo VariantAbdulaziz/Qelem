9973eeb43ea6740a7fc19a909bec24a0bac9542f
hello_world <haileformal@gmail.com>
2022-06-02 09:00:56 +0300

    add upvote and downVote to repository

--- qelem/lib/infrastructure/question/question_repository.dart
+++ qelem/lib/infrastructure/question/question_repository.dart
@@ -3,6 +3,7 @@ import 'package:qelem/infrastructure/question/question_dto.dart';
 import 'package:qelem/infrastructure/question/question_form_dto.dart';
 import 'package:qelem/infrastructure/question/question_mapper.dart';
 
+import '../../common/Vote.dart';
 import '../../domain/question/question.dart';
 import '../../util/either.dart';
 import '../../util/error.dart';
@@ -63,6 +64,98 @@ class QuestionRepository {
       return Either(val: questionDto.toQuestion());
     } on QHttpException catch (e) {
       return Either(error: Error(e.message));
+    }
+  }
+
+  // upVote question
+  Future<Either<Question>> upvoteQuestion(Question question) async {
+    int prevVote;
+    if (question.userVote == Vote.upVote) {
+      prevVote = 1;
+    } else if (question.userVote == Vote.downVote) {
+      prevVote = -1;
+    } else {
+      prevVote = 0;
+    }
+
+    if (question.userVote == Vote.upVote) {
+      try {
+        await questionApi.unvoteQuestion(question.id);
+
+        return Either(
+            val: Question(
+                id: question.id,
+                topic: question.topic,
+                content: question.content,
+                upVotes: question.upVotes - prevVote, // prevVote = 1
+                userVote: Vote.none,
+                downVotes: question.downVotes,
+                author: question.author));
+      } on QHttpException catch (e) {
+        return Either(error: Error(e.message));
+      }
+    } else {
+      try {
+        await questionApi.upVoteQuestion(question.id);
+
+        return Either(
+            val: Question(
+                id: question.id,
+                topic: question.topic,
+                content: question.content,
+                upVotes: question.upVotes + 1,
+                userVote: Vote.upVote,
+                downVotes: question.downVotes + prevVote, // prevVote = -1 or 0
+                author: question.author));
+      } on QHttpException catch (e) {
+        return Either(error: Error(e.message));
+      }
+    }
+  }
+
+  // downVote question
+  Future<Either<Question>> downVoteQuestion(Question question) async {
+    int prevVote;
+    if (question.userVote == Vote.upVote) {
+      prevVote = 1;
+    } else if (question.userVote == Vote.downVote) {
+      prevVote = -1;
+    } else {
+      prevVote = 0;
+    }
+
+    if (question.userVote == Vote.downVote) {
+      try {
+        await questionApi.unvoteQuestion(question.id);
+
+        return Either(
+            val: Question(
+                id: question.id,
+                topic: question.topic,
+                content: question.content,
+                upVotes: question.upVotes,
+                userVote: Vote.none,
+                downVotes: question.downVotes + prevVote, // prevVote == -1 or 0
+                author: question.author));
+      } on QHttpException catch (e) {
+        return Either(error: Error(e.message));
+      }
+    } else {
+      try {
+        await questionApi.downvoteQuestion(question.id);
+
+        return Either(
+            val: Question(
+                id: question.id,
+                topic: question.topic,
+                content: question.content,
+                upVotes: question.upVotes - prevVote, // prevVote == 1 or 0
+                userVote: Vote.downVote,
+                downVotes: question.downVotes + 1,
+                author: question.author));
+      } on QHttpException catch (e) {
+        return Either(error: Error(e.message));
+      }
     }
   }
 }
