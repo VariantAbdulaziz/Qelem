6ce0fd5ee16828710705f38f2abe6fd157c5cd97
Biruk-Tassew <biruktassew21@gmail.com>
2022-06-02 19:28:28 -0400

    added a state file for change_password

---
+++ qelem/lib/application/change_password/change_password_state.freezed.dart
@@ -0,0 +1,600 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+
+part of 'change_password_state.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+mixin _$ChangePasswordState {
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() initial,
+    required TResult Function() loading,
+    required TResult Function() success,
+    required TResult Function(Error error) failure,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult Function()? initial,
+    TResult Function()? loading,
+    TResult Function()? success,
+    TResult Function(Error error)? failure,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? initial,
+    TResult Function()? loading,
+    TResult Function()? success,
+    TResult Function(Error error)? failure,
+    required TResult orElse(),
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(ChangePasswordStateInitial value) initial,
+    required TResult Function(ChangePasswordStateLoading value) loading,
+    required TResult Function(ChangePasswordStateSuccess value) success,
+    required TResult Function(ChangePasswordStateFailure value) failure,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult Function(ChangePasswordStateInitial value)? initial,
+    TResult Function(ChangePasswordStateLoading value)? loading,
+    TResult Function(ChangePasswordStateSuccess value)? success,
+    TResult Function(ChangePasswordStateFailure value)? failure,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(ChangePasswordStateInitial value)? initial,
+    TResult Function(ChangePasswordStateLoading value)? loading,
+    TResult Function(ChangePasswordStateSuccess value)? success,
+    TResult Function(ChangePasswordStateFailure value)? failure,
+    required TResult orElse(),
+  }) =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $ChangePasswordStateCopyWith<$Res> {
+  factory $ChangePasswordStateCopyWith(
+          ChangePasswordState value, $Res Function(ChangePasswordState) then) =
+      _$ChangePasswordStateCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class _$ChangePasswordStateCopyWithImpl<$Res>
+    implements $ChangePasswordStateCopyWith<$Res> {
+  _$ChangePasswordStateCopyWithImpl(this._value, this._then);
+
+  final ChangePasswordState _value;
+  // ignore: unused_field
+  final $Res Function(ChangePasswordState) _then;
+}
+
+/// @nodoc
+abstract class _$$ChangePasswordStateInitialCopyWith<$Res> {
+  factory _$$ChangePasswordStateInitialCopyWith(
+          _$ChangePasswordStateInitial value,
+          $Res Function(_$ChangePasswordStateInitial) then) =
+      __$$ChangePasswordStateInitialCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$$ChangePasswordStateInitialCopyWithImpl<$Res>
+    extends _$ChangePasswordStateCopyWithImpl<$Res>
+    implements _$$ChangePasswordStateInitialCopyWith<$Res> {
+  __$$ChangePasswordStateInitialCopyWithImpl(
+      _$ChangePasswordStateInitial _value,
+      $Res Function(_$ChangePasswordStateInitial) _then)
+      : super(_value, (v) => _then(v as _$ChangePasswordStateInitial));
+
+  @override
+  _$ChangePasswordStateInitial get _value =>
+      super._value as _$ChangePasswordStateInitial;
+}
+
+/// @nodoc
+
+class _$ChangePasswordStateInitial implements ChangePasswordStateInitial {
+  const _$ChangePasswordStateInitial();
+
+  @override
+  String toString() {
+    return 'ChangePasswordState.initial()';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$ChangePasswordStateInitial);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() initial,
+    required TResult Function() loading,
+    required TResult Function() success,
+    required TResult Function(Error error) failure,
+  }) {
+    return initial();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult Function()? initial,
+    TResult Function()? loading,
+    TResult Function()? success,
+    TResult Function(Error error)? failure,
+  }) {
+    return initial?.call();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? initial,
+    TResult Function()? loading,
+    TResult Function()? success,
+    TResult Function(Error error)? failure,
+    required TResult orElse(),
+  }) {
+    if (initial != null) {
+      return initial();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(ChangePasswordStateInitial value) initial,
+    required TResult Function(ChangePasswordStateLoading value) loading,
+    required TResult Function(ChangePasswordStateSuccess value) success,
+    required TResult Function(ChangePasswordStateFailure value) failure,
+  }) {
+    return initial(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult Function(ChangePasswordStateInitial value)? initial,
+    TResult Function(ChangePasswordStateLoading value)? loading,
+    TResult Function(ChangePasswordStateSuccess value)? success,
+    TResult Function(ChangePasswordStateFailure value)? failure,
+  }) {
+    return initial?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(ChangePasswordStateInitial value)? initial,
+    TResult Function(ChangePasswordStateLoading value)? loading,
+    TResult Function(ChangePasswordStateSuccess value)? success,
+    TResult Function(ChangePasswordStateFailure value)? failure,
+    required TResult orElse(),
+  }) {
+    if (initial != null) {
+      return initial(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class ChangePasswordStateInitial implements ChangePasswordState {
+  const factory ChangePasswordStateInitial() = _$ChangePasswordStateInitial;
+}
+
+/// @nodoc
+abstract class _$$ChangePasswordStateLoadingCopyWith<$Res> {
+  factory _$$ChangePasswordStateLoadingCopyWith(
+          _$ChangePasswordStateLoading value,
+          $Res Function(_$ChangePasswordStateLoading) then) =
+      __$$ChangePasswordStateLoadingCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$$ChangePasswordStateLoadingCopyWithImpl<$Res>
+    extends _$ChangePasswordStateCopyWithImpl<$Res>
+    implements _$$ChangePasswordStateLoadingCopyWith<$Res> {
+  __$$ChangePasswordStateLoadingCopyWithImpl(
+      _$ChangePasswordStateLoading _value,
+      $Res Function(_$ChangePasswordStateLoading) _then)
+      : super(_value, (v) => _then(v as _$ChangePasswordStateLoading));
+
+  @override
+  _$ChangePasswordStateLoading get _value =>
+      super._value as _$ChangePasswordStateLoading;
+}
+
+/// @nodoc
+
+class _$ChangePasswordStateLoading implements ChangePasswordStateLoading {
+  const _$ChangePasswordStateLoading();
+
+  @override
+  String toString() {
+    return 'ChangePasswordState.loading()';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$ChangePasswordStateLoading);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() initial,
+    required TResult Function() loading,
+    required TResult Function() success,
+    required TResult Function(Error error) failure,
+  }) {
+    return loading();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult Function()? initial,
+    TResult Function()? loading,
+    TResult Function()? success,
+    TResult Function(Error error)? failure,
+  }) {
+    return loading?.call();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? initial,
+    TResult Function()? loading,
+    TResult Function()? success,
+    TResult Function(Error error)? failure,
+    required TResult orElse(),
+  }) {
+    if (loading != null) {
+      return loading();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(ChangePasswordStateInitial value) initial,
+    required TResult Function(ChangePasswordStateLoading value) loading,
+    required TResult Function(ChangePasswordStateSuccess value) success,
+    required TResult Function(ChangePasswordStateFailure value) failure,
+  }) {
+    return loading(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult Function(ChangePasswordStateInitial value)? initial,
+    TResult Function(ChangePasswordStateLoading value)? loading,
+    TResult Function(ChangePasswordStateSuccess value)? success,
+    TResult Function(ChangePasswordStateFailure value)? failure,
+  }) {
+    return loading?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(ChangePasswordStateInitial value)? initial,
+    TResult Function(ChangePasswordStateLoading value)? loading,
+    TResult Function(ChangePasswordStateSuccess value)? success,
+    TResult Function(ChangePasswordStateFailure value)? failure,
+    required TResult orElse(),
+  }) {
+    if (loading != null) {
+      return loading(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class ChangePasswordStateLoading implements ChangePasswordState {
+  const factory ChangePasswordStateLoading() = _$ChangePasswordStateLoading;
+}
+
+/// @nodoc
+abstract class _$$ChangePasswordStateSuccessCopyWith<$Res> {
+  factory _$$ChangePasswordStateSuccessCopyWith(
+          _$ChangePasswordStateSuccess value,
+          $Res Function(_$ChangePasswordStateSuccess) then) =
+      __$$ChangePasswordStateSuccessCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$$ChangePasswordStateSuccessCopyWithImpl<$Res>
+    extends _$ChangePasswordStateCopyWithImpl<$Res>
+    implements _$$ChangePasswordStateSuccessCopyWith<$Res> {
+  __$$ChangePasswordStateSuccessCopyWithImpl(
+      _$ChangePasswordStateSuccess _value,
+      $Res Function(_$ChangePasswordStateSuccess) _then)
+      : super(_value, (v) => _then(v as _$ChangePasswordStateSuccess));
+
+  @override
+  _$ChangePasswordStateSuccess get _value =>
+      super._value as _$ChangePasswordStateSuccess;
+}
+
+/// @nodoc
+
+class _$ChangePasswordStateSuccess implements ChangePasswordStateSuccess {
+  const _$ChangePasswordStateSuccess();
+
+  @override
+  String toString() {
+    return 'ChangePasswordState.success()';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$ChangePasswordStateSuccess);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() initial,
+    required TResult Function() loading,
+    required TResult Function() success,
+    required TResult Function(Error error) failure,
+  }) {
+    return success();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult Function()? initial,
+    TResult Function()? loading,
+    TResult Function()? success,
+    TResult Function(Error error)? failure,
+  }) {
+    return success?.call();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? initial,
+    TResult Function()? loading,
+    TResult Function()? success,
+    TResult Function(Error error)? failure,
+    required TResult orElse(),
+  }) {
+    if (success != null) {
+      return success();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(ChangePasswordStateInitial value) initial,
+    required TResult Function(ChangePasswordStateLoading value) loading,
+    required TResult Function(ChangePasswordStateSuccess value) success,
+    required TResult Function(ChangePasswordStateFailure value) failure,
+  }) {
+    return success(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult Function(ChangePasswordStateInitial value)? initial,
+    TResult Function(ChangePasswordStateLoading value)? loading,
+    TResult Function(ChangePasswordStateSuccess value)? success,
+    TResult Function(ChangePasswordStateFailure value)? failure,
+  }) {
+    return success?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(ChangePasswordStateInitial value)? initial,
+    TResult Function(ChangePasswordStateLoading value)? loading,
+    TResult Function(ChangePasswordStateSuccess value)? success,
+    TResult Function(ChangePasswordStateFailure value)? failure,
+    required TResult orElse(),
+  }) {
+    if (success != null) {
+      return success(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class ChangePasswordStateSuccess implements ChangePasswordState {
+  const factory ChangePasswordStateSuccess() = _$ChangePasswordStateSuccess;
+}
+
+/// @nodoc
+abstract class _$$ChangePasswordStateFailureCopyWith<$Res> {
+  factory _$$ChangePasswordStateFailureCopyWith(
+          _$ChangePasswordStateFailure value,
+          $Res Function(_$ChangePasswordStateFailure) then) =
+      __$$ChangePasswordStateFailureCopyWithImpl<$Res>;
+  $Res call({Error error});
+}
+
+/// @nodoc
+class __$$ChangePasswordStateFailureCopyWithImpl<$Res>
+    extends _$ChangePasswordStateCopyWithImpl<$Res>
+    implements _$$ChangePasswordStateFailureCopyWith<$Res> {
+  __$$ChangePasswordStateFailureCopyWithImpl(
+      _$ChangePasswordStateFailure _value,
+      $Res Function(_$ChangePasswordStateFailure) _then)
+      : super(_value, (v) => _then(v as _$ChangePasswordStateFailure));
+
+  @override
+  _$ChangePasswordStateFailure get _value =>
+      super._value as _$ChangePasswordStateFailure;
+
+  @override
+  $Res call({
+    Object? error = freezed,
+  }) {
+    return _then(_$ChangePasswordStateFailure(
+      error == freezed
+          ? _value.error
+          : error // ignore: cast_nullable_to_non_nullable
+              as Error,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$ChangePasswordStateFailure implements ChangePasswordStateFailure {
+  const _$ChangePasswordStateFailure(this.error);
+
+  @override
+  final Error error;
+
+  @override
+  String toString() {
+    return 'ChangePasswordState.failure(error: $error)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$ChangePasswordStateFailure &&
+            const DeepCollectionEquality().equals(other.error, error));
+  }
+
+  @override
+  int get hashCode =>
+      Object.hash(runtimeType, const DeepCollectionEquality().hash(error));
+
+  @JsonKey(ignore: true)
+  @override
+  _$$ChangePasswordStateFailureCopyWith<_$ChangePasswordStateFailure>
+      get copyWith => __$$ChangePasswordStateFailureCopyWithImpl<
+          _$ChangePasswordStateFailure>(this, _$identity);
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() initial,
+    required TResult Function() loading,
+    required TResult Function() success,
+    required TResult Function(Error error) failure,
+  }) {
+    return failure(error);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult Function()? initial,
+    TResult Function()? loading,
+    TResult Function()? success,
+    TResult Function(Error error)? failure,
+  }) {
+    return failure?.call(error);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? initial,
+    TResult Function()? loading,
+    TResult Function()? success,
+    TResult Function(Error error)? failure,
+    required TResult orElse(),
+  }) {
+    if (failure != null) {
+      return failure(error);
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(ChangePasswordStateInitial value) initial,
+    required TResult Function(ChangePasswordStateLoading value) loading,
+    required TResult Function(ChangePasswordStateSuccess value) success,
+    required TResult Function(ChangePasswordStateFailure value) failure,
+  }) {
+    return failure(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult Function(ChangePasswordStateInitial value)? initial,
+    TResult Function(ChangePasswordStateLoading value)? loading,
+    TResult Function(ChangePasswordStateSuccess value)? success,
+    TResult Function(ChangePasswordStateFailure value)? failure,
+  }) {
+    return failure?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(ChangePasswordStateInitial value)? initial,
+    TResult Function(ChangePasswordStateLoading value)? loading,
+    TResult Function(ChangePasswordStateSuccess value)? success,
+    TResult Function(ChangePasswordStateFailure value)? failure,
+    required TResult orElse(),
+  }) {
+    if (failure != null) {
+      return failure(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class ChangePasswordStateFailure implements ChangePasswordState {
+  const factory ChangePasswordStateFailure(final Error error) =
+      _$ChangePasswordStateFailure;
+
+  Error get error => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  _$$ChangePasswordStateFailureCopyWith<_$ChangePasswordStateFailure>
+      get copyWith => throw _privateConstructorUsedError;
+}
