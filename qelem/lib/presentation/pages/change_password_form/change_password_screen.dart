ff3c84c568eab7a069982e5114dc306fdd91b237
Biruk-Tassew <biruktassew21@gmail.com>
2022-06-03 07:07:23 -0400

    added bloc in the change password screen

--- qelem/lib/presentation/pages/change_password_form/change_password_screen.dart
+++ qelem/lib/presentation/pages/change_password_form/change_password_screen.dart
@@ -1,5 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+import 'package:qelem/application/change_password/change_password_bloc.dart';
+import 'package:qelem/application/change_password/change_password_event.dart';
+import 'package:qelem/application/change_password/change_password_state.dart';
 import 'package:qelem/common/constants.dart';
+import 'package:qelem/data/local/shared_prefs/shared_prefs_service.dart';
+import 'package:qelem/domain/auth/change_password_form.dart';
+import 'package:qelem/domain/auth/password.dart';
+import 'package:qelem/domain/auth/username.dart';
+import 'package:qelem/infrastructure/auth/auth_api.dart';
+import 'package:qelem/infrastructure/auth/auth_repository.dart';
+import 'package:qelem/presentation/routes/routes.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ChangePasswordScreen extends StatefulWidget {
   const ChangePasswordScreen({Key? key}) : super(key: key);
@@ -12,6 +25,8 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
   bool currentHidden = true;
   bool newHidden = true;
   bool confirmHidden = true;
+  bool _isNetworkInProgress = false;
+  final _formKey = GlobalKey<FormState>();
 
   TextEditingController currentPasswordController = TextEditingController();
   TextEditingController newPasswordController = TextEditingController();
@@ -20,105 +35,169 @@ class _ChangePasswordScreenState extends State<ChangePasswordScreen> {
   @override
   Widget build(BuildContext context) {
     TextStyle? textStyle = Theme.of(context).textTheme.bodyMedium;
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(Constants.changePassword),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
-            child: TextFormField(
-              style: textStyle,
-              obscureText: currentHidden,
-              controller: currentPasswordController,
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return 'Please enter your current password';
-                }
-              },
-              decoration: InputDecoration(
-                  labelText: 'Current Password',
-                  suffixIcon: IconButton(
-                      icon: Icon(currentHidden
-                          ? Icons.visibility
-                          : Icons.visibility_off),
-                      onPressed: () {
-                        setState(() {
-                          currentHidden = !currentHidden;
-                        });
-                      }),
-                  border: const OutlineInputBorder()),
+    return BlocProvider(
+      create: (context) => ChangePasswordBloc(RepositoryProvider.of(context)),
+      child: BlocConsumer<ChangePasswordBloc, ChangePasswordState>(
+        listener: (context, state) {
+          if (state is ChangePasswordStateLoading) {
+            ScaffoldMessenger.of(context).hideCurrentSnackBar();
+            ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(content: Text("Changing password...")));
+            setState(() {
+              _isNetworkInProgress = true;
+            });
+          } else {
+            setState(() {
+              _isNetworkInProgress = false;
+            });
+          }
+          if (state is ChangePasswordStateSuccess) {
+            ScaffoldMessenger.of(context).hideCurrentSnackBar();
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(
+                content: Text('Password changed'),
+                backgroundColor: Colors.green,
+              ),
+            );
+            context.go(Routes.signIn);
+          }
+          if (state is ChangePasswordStateFailure) {
+            ScaffoldMessenger.of(context).hideCurrentSnackBar();
+
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text(state.error.message),
+                backgroundColor: Theme.of(context).errorColor,
+              ),
+            );
+          }
+        },
+        builder: (context, state) {
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text(Constants.changePassword),
             ),
-          ),
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
-            child: TextFormField(
-              style: textStyle,
-              obscureText: newHidden,
-              controller: newPasswordController,
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return 'Please enter your new password';
-                }
-              },
-              decoration: InputDecoration(
-                  labelText: 'New Password',
-                  suffixIcon: IconButton(
-                      icon: Icon(
-                          newHidden ? Icons.visibility : Icons.visibility_off),
-                      onPressed: () {
-                        setState(() {
-                          newHidden = !newHidden;
-                        });
-                      }),
-                  border: const OutlineInputBorder()),
+            body: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Form(
+                    key: _formKey,
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 10.0, horizontal: 15.0),
+                          child: TextFormField(
+                            style: textStyle,
+                            obscureText: currentHidden,
+                            controller: currentPasswordController,
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return 'Please enter your current password';
+                              }
+                            },
+                            decoration: InputDecoration(
+                                labelText: 'Current Password',
+                                suffixIcon: IconButton(
+                                    icon: Icon(currentHidden
+                                        ? Icons.visibility
+                                        : Icons.visibility_off),
+                                    onPressed: () {
+                                      setState(() {
+                                        currentHidden = !currentHidden;
+                                      });
+                                    }),
+                                border: const OutlineInputBorder()),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 10.0, horizontal: 15.0),
+                          child: TextFormField(
+                            style: textStyle,
+                            obscureText: newHidden,
+                            controller: newPasswordController,
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return 'Please enter your new password';
+                              }
+                            },
+                            decoration: InputDecoration(
+                                labelText: 'New Password',
+                                suffixIcon: IconButton(
+                                    icon: Icon(newHidden
+                                        ? Icons.visibility
+                                        : Icons.visibility_off),
+                                    onPressed: () {
+                                      setState(() {
+                                        newHidden = !newHidden;
+                                      });
+                                    }),
+                                border: const OutlineInputBorder()),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 10.0, horizontal: 15.0),
+                          child: TextFormField(
+                            style: textStyle,
+                            obscureText: confirmHidden,
+                            controller: confirmPasswordController,
+                            validator: (value) {
+                              if (value!.isEmpty) {
+                                return 'Please confirm your current password';
+                              } else if (value != newPasswordController.text) {
+                                return 'Passwords don\'t match';
+                              }
+                            },
+                            decoration: InputDecoration(
+                                labelText: 'New Password (again)',
+                                suffixIcon: IconButton(
+                                    icon: Icon(confirmHidden
+                                        ? Icons.visibility
+                                        : Icons.visibility_off),
+                                    onPressed: () {
+                                      setState(() {
+                                        confirmHidden = !confirmHidden;
+                                      });
+                                    }),
+                                border: const OutlineInputBorder()),
+                          ),
+                        ),
+                      ],
+                    )),
+                Container(
+                    margin: const EdgeInsets.only(right: 15.0, top: 15.0),
+                    child: Align(
+                      alignment: Alignment.centerRight,
+                      child: ElevatedButton(
+                          child: Text(
+                            'UPDATE',
+                            style: Theme.of(context).primaryTextTheme.button,
+                          ),
+                          onPressed: _isNetworkInProgress
+                              ? null
+                              : () async {
+                                  if (_formKey.currentState!.validate()) {
+                                    var passwordForm = ChangePasswordForm(
+                                      //TODO get the username
+                                      userName: UserName("test"),
+                                      currentPassword: Password(
+                                          currentPasswordController.text),
+                                      newPassword:
+                                          Password(newPasswordController.text),
+                                    );
+
+                                    BlocProvider.of<ChangePasswordBloc>(context)
+                                        .add(ChangePasswordEventChangePassword(
+                                            passwordForm));
+                                  }
+                                }),
+                    ))
+              ],
             ),
-          ),
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
-            child: TextFormField(
-              style: textStyle,
-              obscureText: confirmHidden,
-              controller: confirmPasswordController,
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return 'Please confirm your current password';
-                } else if (value != newPasswordController.text) {
-                  return 'Passwords don\'t match';
-                }
-              },
-              decoration: InputDecoration(
-                  labelText: 'New Password (again)',
-                  suffixIcon: IconButton(
-                      icon: Icon(confirmHidden
-                          ? Icons.visibility
-                          : Icons.visibility_off),
-                      onPressed: () {
-                        setState(() {
-                          confirmHidden = !confirmHidden;
-                        });
-                      }),
-                  border: const OutlineInputBorder()),
-            ),
-          ),
-          Container(
-              margin: const EdgeInsets.only(right: 15.0, top: 15.0),
-              child: Align(
-                alignment: Alignment.centerRight,
-                child: ElevatedButton(
-                  child: Text(
-                    'UPDATE',
-                    style: Theme.of(context).primaryTextTheme.button,
-                  ),
-                  onPressed: () async {},
-                ),
-              ))
-        ],
+          );
+        },
       ),
     );
   }
