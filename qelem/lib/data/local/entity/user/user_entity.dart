9939550d5dfcfc6d4aee2c86aceef81dfcd810ed
hello_world <haileformal@gmail.com>
2022-06-04 06:19:46 +0300

    add user entity

---
+++ qelem/lib/data/local/entity/user/user_entity.dart
@@ -0,0 +1,18 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'user_entity.freezed.dart';
+part 'user_entity.g.dart';
+
+@freezed
+class UserEntity with _$UserEntity {
+
+  const factory UserEntity({
+   required int id,
+    required String userName,
+    required String firstName,
+    required String lastName,
+    required String profilePicture,
+  }) = _UserEntity;
+
+  factory UserEntity.fromJson(Map<String, dynamic> json) => _$UserEntityFromJson(json);
+}
