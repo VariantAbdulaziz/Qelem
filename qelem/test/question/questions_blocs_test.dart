5618a6cce1367bd07e1c9b69f3841fa14201f0aa
Abdulaziz Ali <s9uidward@gmail.com>
2022-06-05 08:30:13 +0300

    keeping it tidy solved load asset issue

--- qelem/test/question/questions_blocs_test.dart
+++ qelem/test/question/questions_blocs_test.dart
@@ -5,12 +5,16 @@ import 'package:mockito/mockito.dart';
 import 'package:qelem/application/question/question_detail/question_detail_bloc.dart';
 import 'package:qelem/application/question/question_detail/question_detail_event.dart';
 import 'package:qelem/application/question/question_detail/question_detail_state.dart';
+import 'package:qelem/application/question/questions_list/questions_list_bloc.dart';
+import 'package:qelem/application/question/questions_list/questions_list_event.dart';
+import 'package:qelem/application/question/questions_list/questions_list_state.dart';
+import 'package:qelem/domain/common/vote.dart';
 import 'package:qelem/infrastructure/question/question_repository.dart';
 import 'package:qelem/util/either.dart';
 import 'package:qelem/util/error.dart';
 
-import 'question_detail_bloc_test.mocks.dart';
 import 'questions_tests_util.dart';
+import 'questions_blocs_test.mocks.dart';
 
 @GenerateMocks([QuestionRepository])
 void main() {
@@ -21,12 +25,51 @@ void main() {
     mockQuestionRepository = MockQuestionRepository();
   });
 
+  group("QuestionsListsBloc", () {
+    test(
+        "should emit [QuestionListStateLoading, QuestionListStateSuccess] when event QuestionsListEventLoad is called and no error occur",
+        () async {
+      final mockQuestions = List.generate(3, (index) => mockQuestion);
+
+      when(mockQuestionRepository.getMyQuestions())
+          .thenAnswer((_) async => Either(val: mockQuestions));
+
+      final questionsListBloc =
+          QuestionsListBloc(questionRepository: mockQuestionRepository);
+
+      questionsListBloc.add(const QuestionsListEventLoad());
+
+      final expected = [
+        const QuestionsListStateLoading(),
+        QuestionsListStateSuccess(mockQuestions),
+      ];
+
+      expectLater(questionsListBloc.stream, emitsInOrder(expected));
+    });
+
+    test(
+        "should emit [QuestionListStateLoading, QuestionListStateError] when event QuestionsListEventLoad is called and an error occurs",
+        () async {
+      when(mockQuestionRepository.getMyQuestions())
+          .thenAnswer((_) async => Either(error: Error('error')));
+
+      final questionsListBloc =
+          QuestionsListBloc(questionRepository: mockQuestionRepository);
+
+      questionsListBloc.add(const QuestionsListEventLoad());
+
+      final expected = [
+        const QuestionsListStateLoading(),
+        QuestionsListStateError(Error('error')),
+      ];
+      expectLater(questionsListBloc.stream, emitsInOrder(expected));
+    });
+  });
+
   group("QuestionDetailBloc", () {
     test(
         "should emit [QuestionDetailStateLoading, QuestionDetailStateLoadedQuestion] when event QuestionDetailLoadEvent is called and no error occur",
         () async {
-      final mockQuestion = await getQuestion();
-
       when(mockQuestionRepository.getQuestionById(mockQuestion.id))
           .thenAnswer((_) async => Either(val: mockQuestion));
 
@@ -93,6 +136,50 @@ void main() {
 
       final expected = [
         const QuestionDetailStateLoading(),
+        QuestionDetailStateError(Error('error')),
+      ];
+
+      expectLater(questionDetailBloc.stream, emitsInOrder(expected));
+    });
+
+    test(
+        "should emit [QuestionDetailStateLoadedQuestion] when event VoteQuestionEvent is called and no error occurs",
+        () async {
+      final mockQuestion = await getQuestion();
+
+      when(mockQuestionRepository.getQuestionById(mockQuestion.id))
+          .thenAnswer((_) async => Either(val: mockQuestion));
+      when(mockQuestionRepository.voteQuestion(mockQuestion.id, Vote.upVote))
+          .thenAnswer((_) async => Either(val: mockQuestion));
+
+      final questionDetailBloc =
+          QuestionDetailBloc(questionRepository: mockQuestionRepository);
+
+      questionDetailBloc.add(VoteQuestionEvent(mockQuestion, Vote.upVote));
+
+      final expected = [
+        QuestionDetailStateLoadedQuestion(mockQuestion),
+      ];
+
+      expectLater(questionDetailBloc.stream, emitsInOrder(expected));
+    });
+
+    test(
+        "should emit [QuestionDetailStateLoadedQuestion] when event VoteQuestionEvent is called and an error occurs",
+        () async {
+      final mockQuestion = await getQuestion();
+
+      when(mockQuestionRepository.getQuestionById(mockQuestion.id))
+          .thenAnswer((_) async => Either(val: mockQuestion));
+      when(mockQuestionRepository.voteQuestion(mockQuestion.id, Vote.upVote))
+          .thenAnswer((_) async => Either(error: Error('error')));
+
+      final questionDetailBloc =
+          QuestionDetailBloc(questionRepository: mockQuestionRepository);
+
+      questionDetailBloc.add(VoteQuestionEvent(mockQuestion, Vote.upVote));
+
+      final expected = [
         QuestionDetailStateError(Error('error')),
       ];
 
--- qelem/test/question/questions_tests_util.dart
+++ qelem/test/question/questions_tests_util.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 
 import 'package:flutter/services.dart';
+import 'package:qelem/domain/auth/user.dart';
+import 'package:qelem/domain/common/vote.dart';
 import 'package:qelem/domain/question/question.dart';
 import 'package:qelem/infrastructure/question/question_dto.dart';
 import 'package:qelem/infrastructure/question/question_mapper.dart';
@@ -21,3 +23,20 @@ Future<Question> getQuestion() async {
 Future<String> _loadAsset(String path) async {
   return rootBundle.loadString(path);
 }
+
+final mockQuestion = Question(
+  id: 6,
+  topic: 'asdfad',
+  content: 'this is the question\'s content',
+  author: User(
+      id: 3,
+      firstName: "bruk",
+      lastName: "tedla",
+      userName: "brukted",
+      profilePicture: "/user-photos/1/abebe.jpeg"),
+  createdAt: DateTime.parse("2022-05-30 13:14:18.582299z"),
+  updatedAt: DateTime.parse("2022-05-30 13:14:18.582299z"),
+  upVotes: 0,
+  downVotes: 0,
+  userVote: Vote.upVote,
+);
