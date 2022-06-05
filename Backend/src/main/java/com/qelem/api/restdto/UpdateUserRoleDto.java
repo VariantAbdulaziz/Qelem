ce50e4aa19ee16e369b6417a9b412ff3b9df50bf
Bruk Tedla <biruk.tedla93@gmail.com>
2022-06-05 18:44:45 +0300

    add admin endpoints

---
+++ Backend/src/main/java/com/qelem/api/restdto/UpdateUserRoleDto.java
@@ -0,0 +1,13 @@
+package com.qelem.api.restdto;
+
+public class UpdateUserRoleDto {
+    private String role;
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
